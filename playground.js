e97671231cc676972f363b45daf5005741249066
Zacharyjpeter <zacharyjpeter@gmail.com>
2023-03-08 13:28:21 -0600

    just getting started

---
+++ playground.js
@@ -0,0 +1,21 @@
+// This is an in-line comment.
+
+/* This is a multi-line comment */
+
+var myName;
+
+// Setup
+var a;
+
+// Only change code below this line
+
+a = 7;
+
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+
+b = a;
