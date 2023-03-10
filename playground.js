0fd64898781638ad81759fe11c1be4fab0fa285d
Zacharyjpeter <zacharyjpeter@gmail.com>
2023-03-09 23:04:48 -0600

    this is pretty cool

--- playground.js
+++ playground.js
@@ -19,3 +19,21 @@ var b;
 // Only change code below this line
 
 b = a;
+
+//Lesson four. You can assign variables in the same line of code
+var a = 9;
+
+//Lesson five. You can assign a string to a variable
+var myFirstName = "Zachary";
+var myLastName = "Peterson";
+
+//Lesson six. An empty variable is an undefined variable
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
