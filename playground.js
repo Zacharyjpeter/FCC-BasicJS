027877100691b4616cdcb60f65200bcfe4ea4b60
Zacharyjpeter <zacharyjpeter@gmail.com>
2023-03-15 22:41:19 -0500

    this feels like a math class

--- playground.js
+++ playground.js
@@ -82,3 +82,7 @@ const ourDecimal = 5.7;
 
 // Only change code below this line
 let myDecimal = 5.7;
+
+//More numerical inputs. Yay!
+const product = 2.0 * 2.5;
+const quotient = 4.4 / 2.0; // Change this line
